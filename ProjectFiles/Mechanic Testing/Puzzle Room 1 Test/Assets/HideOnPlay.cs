36d46a6d685d620dacc59c9a6baa136a1acbe9bd
jordan akehurst <jordanakehurst@gmail.com>
2022-06-01 12:47:07 +0100

    12 Second Puzzle

---
+++ ProjectFiles/Mechanic Testing/Puzzle Room 1 Test/Assets/HideOnPlay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnPlay : MonoBehaviour
+{
+    public bool ignore;
+    Color thisColour;
+    SpriteRenderer thisSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisSprite = GetComponent<SpriteRenderer>();
+        thisColour = thisSprite.color;
+        thisColour.a = 0;
+        thisSprite.color = thisColour;
+    }
+    private void Update()
+    {
+        if (name.Contains("PCollision")) transform.localPosition = new Vector3(0, -0.72f, 0);
+        if (!ignore)
+        {
+            thisColour.a = 0;
+        }
+        else
+        {
+            thisColour.a = 1;
+        }
+
+        thisSprite.color = thisColour;
+    }
+}
